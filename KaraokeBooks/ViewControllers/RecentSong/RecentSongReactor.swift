fb0209e9cf2cd918df9fd75f9769ac90405ffcfb
JustHm <taggma12@gmail.com>
2024-12-17 15:22:32 +0900

    최신곡 검색 템플릿만 완성, 최신곡 검색 API 수정되면 업데이트 예정

---
+++ KaraokeBooks/ViewControllers/RecentSong/RecentSongReactor.swift
@@ -0,0 +1,29 @@
+//
+//  RecentSongReactor.swift
+//  KaraokeBooks
+//
+//  Created by 안정흠 on 12/12/24.
+//
+
+import Foundation
+import ReactorKit
+
+final class RecentSongReactor: Reactor {
+    var initialState = State()
+    
+    
+    enum Action {
+    }
+    enum Mutation {
+    }
+    struct State {
+    }
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        
+    }
+}
